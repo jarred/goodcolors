e8ac187189cab3cb43d5d99a7de93677c7194779
PrototypeAlex <darth.gibson@gmail.com>
2016-10-29 09:01:25 +1300

    Refactor combination to split things up a bit

--- app/tool/combination.js
+++ app/tool/combination.js
@@ -2,63 +2,105 @@ import React from 'react';
 import Color from 'color';
 import TopStories from 'json!../data/nyt-top-stories.json'; // eslint-disable-line import/no-extraneous-dependencies
 
+const OptionSwatch = ({color}) => {
+  const swatchStyle = {
+    backgroundColor: color.hexString(),
+    color: 'rgba(255,255,255,.5)',
+    fontSize: '11px'
+  };
+  if (color.light()) {
+    swatchStyle.color = 'rgba(0,0,0,.5)';
+  }
+  return <span className="dib pa2 br2" style={swatchStyle}>{color.hexString()}</span>;
+};
+
+OptionSwatch.propTypes = {
+  color: React.PropTypes.object.isRequired
+};
+
+const BaseSwatch = ({color}) => {
+  const baseSwatchStyle = {
+    fontSize: '11px',
+    border: '1px solid rgba(0,0,0,.1)',
+    color: 'rgba(0,0,0,.5)'
+  };
+  if (color.dark()) {
+    baseSwatchStyle.border = '1px solid rgba(255,255,255,.1)';
+    baseSwatchStyle.color = 'rgba(255,255,255,.5)';
+  }
+  return (
+    <span className="border-box dib pa2 br2" style={baseSwatchStyle}>{color.hexString()}</span>
+  );
+};
+
+BaseSwatch.propTypes = {
+  color: React.PropTypes.object.isRequired
+};
+
+const ComboDisplay = props => {
+  const {title, bodyColor, headingColor, baseColor, abstract} = props;
+  return (
+    <div className="fl w-100 w-100-m w-50-ns pa3 pa4-ns">
+      <h2 style={{color: headingColor.rgbString()}} className="mb2 ma0">{title}</h2>
+      <p style={{color: bodyColor.rgbString()}} className="mt2 mb2 f6 lh-copy">
+        {abstract}
+      </p>
+      <div className="mt3">
+        <BaseSwatch color={baseColor}/>
+        <OptionSwatch color={headingColor}/>
+        <OptionSwatch color={bodyColor}/>
+      </div>
+    </div>
+  );
+};
+
+ComboDisplay.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  abstract: React.PropTypes.string.isRequired,
+  bodyColor: React.PropTypes.object,
+  headingColor: React.PropTypes.object,
+  baseColor: React.PropTypes.object
+};
+
+function getDefaultColorOption(baseColor, options) {
+  if (options.length > 0) {
+    return options[0];
+  }
+
+  if (baseColor.light()) {
+    return Color('rgba(0,0,0,.4)');
+  }
+
+  return Color('rgba(255,255,255,.4)');
+}
+
 class Combination extends React.Component {
   constructor(props) {
     super(props);
     this.handleSetBodyColor = this.handleSetBodyColor.bind(this);
     this.handleSetHeadingColor = this.handleSetHeadingColor.bind(this);
     const story = TopStories.results[Math.floor(Math.random() * TopStories.results.length)];
-    this.state = {
-      base: props.combos.base,
-      headings: props.combos.headings,
-      body: props.combos.body,
-      headingColor: null,
-      bodyColor: null,
-      titleText: story.title,
-      bodyText: story.abstract
-    };
+    const {body, headings, base} = props.combos;
+    const headingColor = getDefaultColorOption(base, headings);
+    const bodyColor = getDefaultColorOption(base, body);
+
+    this.state = {base, headings, body, story, bodyColor, headingColor};
   }
 
   handleSetHeadingColor(event) {
     const el = event.target;
-    const c = el.getAttribute('data-value');
+    const c = Color(el.getAttribute('data-value'));
     this.setState({headingColor: c});
   }
 
   handleSetBodyColor(event) {
     const el = event.target;
-    const c = el.getAttribute('data-value');
+    const c = Color(el.getAttribute('data-value'));
     this.setState({bodyColor: c});
   }
 
   render() {
     const style = {backgroundColor: this.state.base.rgbString()};
-
-    let headingColor = 'rgba(255,255,255,.4)';
-    if (this.state.base.light()) {
-      headingColor = 'rgba(0,0,0,.4)';
-    }
-
-    if (this.state.headings.length > 0) {
-      headingColor = this.state.headings[0].rgbString();
-    }
-
-    let bodyColor = 'rgba(255,255,255,.4)';
-    if (this.state.base.light()) {
-      bodyColor = 'rgba(0,0,0,.4)';
-    }
-
-    if (this.state.body.length > 0) {
-      bodyColor = this.state.body[0].rgbString();
-    }
-
-    if (this.state.headingColor !== null) {
-      headingColor = this.state.headingColor;
-    }
-
-    if (this.state.bodyColor !== null) {
-      bodyColor = this.state.bodyColor;
-    }
 
     let headingText = 'No accessible heading options found. Try adding more colors!';
     if (this.state.headings.length === 1) {
@@ -74,55 +116,18 @@ class Combination extends React.Component {
       bodyText = this.state.body.length + ' options found.';
     }
 
-    let c = Color(headingColor); // eslint-disable-line babel/new-cap
-    let swatchStyle = {
-      backgroundColor: c.hexString(),
-      color: 'rgba(255,255,255,.5)',
-      fontSize: '11px'
-    };
-    if (c.light()) {
-      swatchStyle.color = 'rgba(0,0,0,.5)';
-    }
-    const headingSwatch = <span className="dib pa2 br2" style={swatchStyle}>{c.hexString()}</span>;
-
-    c = Color(bodyColor); // eslint-disable-line babel/new-cap
-    swatchStyle = {
-      backgroundColor: c.hexString(),
-      color: 'rgba(255,255,255,.4)',
-      fontSize: '11px'
-    };
-
-    if (c.light()) {
-      swatchStyle.color = 'rgba(0,0,0,.4)';
-    }
-    const bodySwatch = <span className="dib pa2 br2" style={swatchStyle}>{c.hexString()}</span>;
-
-    const baseSwatchStyle = {
-      color: this.state.base.hexString(),
-      fontSize: '11px',
-      border: '1px solid rgba(0,0,0,.1)'
-      // color: 'rgba(0,0,0,.5)'
-    };
-    if (this.state.base.dark()) {
-      baseSwatchStyle.border = '1px solid rgba(255,255,255,.1)';
-      baseSwatchStyle.color = 'rgba(255,255,255,.5)';
-    }
-    const baseSwatch = <span className="border-box dib pa2 br2" style={baseSwatchStyle}>{this.state.base.hexString()}</span>;
-
     return (
       <div className="db" style={style}>
-        <div className="fl w-100 w-100-m w-50-ns pa3 pa4-ns">
-          <h2 style={{color: headingColor}} className="mb2 ma0">{this.state.titleText}</h2>
-          <p style={{color: bodyColor}} className="mt2 mb2 f6 lh-copy">
-            {this.state.bodyText}
-          </p>
-          <div className="mt3">
-            {baseSwatch} {headingSwatch} {bodySwatch}
-          </div>
-        </div>
+        <ComboDisplay
+          title={this.state.story.title}
+          abstract={this.state.story.abstract}
+          headingColor={this.state.headingColor}
+          bodyColor={this.state.bodyColor}
+          baseColor={this.state.base}
+          />
 
         <div className="fl w-100 w-100-m w-50-ns pa3 pa4-ns">
-          <span style={{color: headingColor}} className="db mb2 f6">{headingText}</span>
+          <span style={{color: this.state.headingColor.rgbString()}} className="db mb2 f6">{headingText}</span>
           <div className="mr5-ns">
             {this.state.headings.map(result => (
               <div className="fl db br1 mr1 mb1" key={result.hexString().replace('#', '')} style={{width: '30px', backgroundColor: result.rgbString()}}>
@@ -133,7 +138,7 @@ class Combination extends React.Component {
             ))}
           </div>
           <div className="cf"/>
-          <span style={{color: bodyColor}} className="db mb2 f6 mt3">{bodyText}</span>
+          <span style={{color: this.state.bodyColor.rgbString()}} className="db mb2 f6 mt3">{bodyText}</span>
           {this.state.body.map(result => (
             <div className="fl db br1 mr1 mb1" key={result.hexString().replace('#', '')} style={{width: '30px', backgroundColor: result.rgbString()}}>
               <div className="aspect-ratio aspect-ratio--1x1">
@@ -150,7 +155,7 @@ class Combination extends React.Component {
 }
 
 Combination.propTypes = {
-  combos: React.PropTypes.array.isRequired
+  combos: React.PropTypes.object.isRequired
 };
 
 export default Combination;
